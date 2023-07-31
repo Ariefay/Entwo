315f73d676de249f17c176e64971927423e41bcc
Ariefay <125006355+Ariefay@users.noreply.github.com>
2023-07-31 14:26:16 +0700

    Software serial with ESP32 and Module MP3-TF-16P

--- dfPlayerWithSoftwareSerial/dfPlayerWithSoftwareSerial.ino
+++ dfPlayerWithSoftwareSerial/dfPlayerWithSoftwareSerial.ino
@@ -1,15 +1,13 @@
 #include "SoftwareSerial.h"
 #include "DFRobotDFPlayerMini.h"
+#include "Arduino.h"
 
+EspSoftwareSerial::UART swSer;
+constexpr uint32_t TESTBPS = 9600;
 //Declaration MP3 Module
-HardwareSerial Serial_df(2);
+//HardwareSerial Serial_df(2);
 DFRobotDFPlayerMini myDFPlayer;
 
-// #if defined (ESP32)
-// #define RX (18)     //GPIO18 to TX
-// #define TX (19)     //GPIO19 to RX
-//#define rxPin 18
-//#define txPin 19
 
 //Declaration Parsing
 String inputString;
@@ -20,39 +18,33 @@ void SerialEventHandler();
 void commandEvent(String data);
 
 
-// Set up a new SoftwareSerial object
-SoftwareSerial mod(18, 19);
-//SoftwareSerial mySerial (rxPin, txPin);
  
 
 void setup() {
 //Inisialisasi Serial Monitor
-    Serial_df.begin(9600);
-    Serial.begin(115200);
-    
-    // pinMode(rxPin, INPUT);
-    // pinMode(txPin, OUTPUT);
-    // mySerial.begin(115200);
+    //Serial_df.begin(9600);
+    Serial.begin(9600);
+    swSer.begin(TESTBPS,EspSoftwareSerial::SWSERIAL_8N1, 18, 19 );
 //Correction MP3 Module 
-    if (!myDFPlayer.begin(Serial_df, false))
+    // if (!myDFPlayer.begin(Serial_df, false))
+    if (!myDFPlayer.begin(swSer, false))
   {
 //MP3 Module No Detection
     Serial.println(F("Not initialized:"));
     Serial.println(F("1. Check the DFPlayer Mini connections"));
     Serial.println(F("2. Insert an SD card"));
-    //für esp8266
+    
     //while(true);
 //MP3 Module Detection
   }else Serial.println(F("DFPlayer Mini online."));
   
-  //  myDFPlayer.volume(100);
-  //  myDFPlayer.play();
-
 }
+
  
 void loop() {
 
 //Function Called
+
  SerialEventHandler();
 
 
@@ -60,7 +52,7 @@ void loop() {
 }
 //Detection Data Input
 void SerialEventHandler(){
-//
+
 //Collection Data Input
  if(Serial.available ()> 0){
     while (Serial.available()){
@@ -99,6 +91,7 @@ void commandEvent(String data){
   Serial.print("Order Record Number =");
   Serial.println(pinOrder);
 
+  
 //Parsing Work
   if(command == "STOP"){
     Serial.println("MP3 STOP");
@@ -113,6 +106,10 @@ void commandEvent(String data){
     else if (pinOrder == 2){
     myDFPlayer.volume(20);
     myDFPlayer.play(2);
+    }
+    else if (pinOrder == 3){
+    myDFPlayer.volume(20);
+    myDFPlayer.play(3);
     }
   }
   else {
